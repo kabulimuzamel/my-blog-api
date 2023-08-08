04f676c98371335d2596d8b09b2a710f32a2983b
ZCP1 <zcp1@ZCP1s-MacBook-Pro.local>
2023-08-08 17:28:08 -0400

    For Review

--- src/userComponent/ProtectedRoutes.js
+++ src/userComponent/ProtectedRoutes.js
@@ -1,25 +1,44 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useTransition} from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 
 export function ProtectedRoutes() {
     const [token, setToken] = useState(undefined);
-    useEffect(() => {
-        setTimeout(() => {
-            if(localStorage.getItem('token')) {
-                fetch(`http://localhost:3000/api/user/${localStorage.getItem('token')}`)
-                    .then(res => {
-                        if(res.status === 200) {
-                            setToken(localStorage.getItem('token'));
-                        } else {
-                            setToken(false)
-                        }
-                    })
-            } 
-            }, 1000)
-    }, []);
+    const [pending, startTransition] = useTransition();
+    
+    const spinnerStyle = {
+        position: 'absolute',
+        top: '10rem',
+        right: '43rem',
+        height: '3rem',
+        width: '3rem'
+    }
 
+    useEffect(() => {
+        setTimeout(() => {        
+            startTransition(() => {
+                if(localStorage.getItem('token')) {
+                    fetch(`http://localhost:3000/api/user/${localStorage.getItem('token')}`)
+                        .then(res => {
+                            if(res.status === 200) {
+                                setToken(localStorage.getItem('token'));
+                            } else {
+                                setToken(false);
+                            }
+                        })
+                } else {
+                    setToken(false)
+                } 
+            })
+        }, 1000);
+    }, []);
+    
     if(token === undefined) {
-        return null;
+        return (
+            <div className="position-relative">
+                <Spinner style={spinnerStyle} animation="border" variant="light" />
+            </div>
+        )
     }
     if(!token) {
         return <Navigate to="/LoginPage" />
